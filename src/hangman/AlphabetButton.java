10d1ed6c1af54612d86c510823a75a7e2934b27a
Dimitris Doukas <dimitrisdoukas@Dimitriss-MacBook-Air-2.local>
2024-03-20 19:59:19 +0000

    added comments

--- src/hangman/AlphabetButton.java
+++ src/hangman/AlphabetButton.java
@@ -3,14 +3,18 @@ package hangman;
 import javax.swing.JButton;
 
 public class AlphabetButton extends JButton {
-	private int buttonPosition;
-	
-	public AlphabetButton(String buttonText, int index) {
-		super(buttonText);
-		this.buttonPosition = index;
-	}
-	
-	public int getButtonPosition() {
-		return this.buttonPosition;
-	}
+    // The position of this button in array
+    private int buttonPosition;
+
+    public AlphabetButton(String buttonText, int index) {
+        // Call the constructor of the superclass (JButton) to set Text in button
+        super(buttonText);
+        // Set the position of this button
+        this.buttonPosition = index;
+    }
+
+    public int getButtonPosition() {
+        // Return the value of the buttonPosition instance variable
+        return this.buttonPosition;
+    }
 }
--- src/hangman/ButtonPanel.java
+++ src/hangman/ButtonPanel.java
@@ -1,30 +1,47 @@
 package hangman;
 
 import java.awt.GridLayout;
+import javax.swing.JPanel;
+import javax.swing.JButton;
 
-import javax.swing.*;
-
+/**
+ * A ButtonPanel is a JPanel that contains an array of AlphabetButtons.
+ * It is used to display the letters of the alphabet in a Hangman
+ */
 public class ButtonPanel extends JPanel {
-	private AlphabetButton[] alphabetButtons;
-	private char[] alphabet;
-	
-	public ButtonPanel() {
-		this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
-		this.alphabetButtons = new AlphabetButton[this.alphabet.length];
-		
-		this.setLayout(new GridLayout(2, this.alphabet.length/2));
-		
-		for(int i = 0; i < this.alphabet.length; i++) {
-			this.alphabetButtons[i] = new AlphabetButton(String.valueOf(this.alphabet[i]), i);
-			this.add(alphabetButtons[i]);
-		}
-	}
-	
-	public int getNumberOfButtons() {
-		return this.alphabetButtons.length;
-	}
-	
-	public JButton getButtonAt(int index) {
-		return this.alphabetButtons[index];
-	}
+    // The array of AlphabetButtons
+    private AlphabetButton[] alphabetButtons;
+    // The array of letters in the alphabet
+    private char[] alphabet;
+
+    public ButtonPanel() {
+        // Initialize the alphabet array with the uppercase letters of the alphabet
+        this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+
+        // Initialize the AlphabetButton array with the same length as the alphabet array
+        this.alphabetButtons = new AlphabetButton[this.alphabet.length];
+
+        // Set the layout of this JPanel to a 2-row grid with columns determined by the length of the alphabet array divided by 2
+        this.setLayout(new GridLayout(2, this.alphabet.length/2));
+
+        // Loop through the alphabet array
+        for(int i = 0; i < this.alphabet.length; i++) {
+
+            // Create a new AlphabetButton with the current letter and its position in the alphabet array
+            this.alphabetButtons[i] = new AlphabetButton(String.valueOf(this.alphabet[i]), i);
+
+            // Add the AlphabetButton to this JPanel
+            this.add(alphabetButtons[i]);
+        }
+    }
+
+    public int getNumberOfButtons() {
+        // Return the length of the AlphabetButton array
+        return this.alphabetButtons.length;
+    }
+
+    public JButton getButtonAt(int index) {
+        // Return the AlphabetButton at the given index
+        return this.alphabetButtons[index];
+    }
 }
